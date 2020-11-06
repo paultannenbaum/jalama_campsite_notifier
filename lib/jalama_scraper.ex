6fbeb2bc530d67cba968493e63997d611c621bfc
Paul Tannenbaum <paultannenbaum@gmail.com>
2020-11-06 12:20:45 -0800

    Init project

---
+++ lib/jalama_scraper.ex
@@ -0,0 +1,18 @@
+defmodule JalamaScraper do
+  @moduledoc """
+  Documentation for `JalamaScraper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JalamaScraper.hello()
+      :world
+
+  """
+  def hello do
+    IO.puts('Hello World')
+  end
+end
---
+++ test/jalama_scraper_test.exs
@@ -0,0 +1,8 @@
+defmodule JalamaScraperTest do
+  use ExUnit.Case
+  doctest JalamaScraper
+
+  test "greets the world" do
+    assert JalamaScraper.hello() == :world
+  end
+end
