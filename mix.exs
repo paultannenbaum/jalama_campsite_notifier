b6a17502c018867ff347cf6592b1a9c9695ad803
Paul Tannenbaum <paultannenbaum@gmail.com>
2020-11-12 14:23:28 -0800

    Add packages needed for scrapper

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule JalamaScrapper.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:mechanize, "~> 0.1"},
+      {:timex, "~> 3.5"}
     ]
   end
 
